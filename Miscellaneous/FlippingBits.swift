1baac3e5ac1231aef6f62e35b475bd1e8e7da35e
Fikri Kurtulus <fikrikurtulus53@gmail.com>
2019-09-17 21:14:26 +0300

    Add Flipping Bits solution

---
+++ Miscellaneous/FlippingBits.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+// Complete the flippingBits function below.
+func flippingBits(n: Int) -> Int {
+    return Int(~UInt32(n))
+}
+
+let stdout = ProcessInfo.processInfo.environment["OUTPUT_PATH"]!
+FileManager.default.createFile(atPath: stdout, contents: nil, attributes: nil)
+let fileHandle = FileHandle(forWritingAtPath: stdout)!
+
+guard let q = Int((readLine()?.trimmingCharacters(in: .whitespacesAndNewlines))!)
+    else { fatalError("Bad input") }
+
+for qItr in 1...q {
+    guard let n = Int((readLine()?.trimmingCharacters(in: .whitespacesAndNewlines))!)
+        else { fatalError("Bad input") }
+    
+    let result = flippingBits(n: n)
+    
+    fileHandle.write(String(result).data(using: .utf8)!)
+    fileHandle.write("\n".data(using: .utf8)!)
+}
